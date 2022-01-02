7a39e52fea271f2d6efcfe88d3d73d8b3b586787
Gamerliterall <94064394+Gamerliterall@users.noreply.github.com>
2022-01-02 12:35:31 +0530

    Add files via upload

---
+++ style.cs
@@ -0,0 +1,20 @@
+
+h1
+{
+	color: red; 
+	font-size: 40px;
+}
+.body_backgorund {
+	background-image: url("b1.jpg");
+	background-position: center;
+	background-size: cover;
+}
+
+#my_canvas{
+    background color: blue;
+	border-width: 10px;
+	border-style: ridge;
+}
+span{
+	font-size: 20px;
+}
